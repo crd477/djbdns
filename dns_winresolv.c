f5bc07840c7f511af4b5cbd2240f8ee3a8689af5
Chad Dougherty <crd@acm.org>
2016-08-20 11:53:23 -0400

    Fix return values and change the logic to return the last IP address based on some observations of IPs from inactive adapters still being returned.

--- dns_winresolv.c
+++ dns_winresolv.c
@@ -1,15 +1,11 @@
 /* cribbed from:
-   https://msdn.microsoft.com/en-us/library/windows/desktop/aa365968%28v=vs.85%29.aspx
-*/
-#include <winsock2.h>
+ https://msdn.microsoft.com/en-us/library/windows/desktop/aa365968%28v=vs.85%29.aspx
+ */
 #include <iphlpapi.h>
-#include <windows.h>
 #pragma comment(lib, "IPHLPAPI.lib")
 
 #define MALLOC(x) HeapAlloc(GetProcessHeap(), 0, (x))
 #define FREE(x) HeapFree(GetProcessHeap(), 0, (x))
-
-/* Note: could also use malloc() and free() */
 
 extern char *dns_winresolv() {
 
@@ -17,32 +13,43 @@ extern char *dns_winresolv() {
   ULONG ulOutBufLen;
   DWORD dwRetVal;
   IP_ADDR_STRING *pIPAddr;
-  char *x;
+  char *r;
 
   pFixedInfo = (FIXED_INFO *)MALLOC(sizeof(FIXED_INFO));
   if (pFixedInfo == NULL) {
-    return -1;
+    return NULL;
   }
   ulOutBufLen = sizeof(FIXED_INFO);
 
   /* Make an initial call to GetNetworkParams to get
-     the necessary size into the ulOutBufLen variable */
+   the necessary size into the ulOutBufLen variable */
   if (GetNetworkParams(pFixedInfo, &ulOutBufLen) == ERROR_BUFFER_OVERFLOW) {
     FREE(pFixedInfo);
     pFixedInfo = (FIXED_INFO *)MALLOC(ulOutBufLen);
     if (pFixedInfo == NULL) {
-      return -1;
+      return NULL;
     }
   }
 
+  /* NOTE: this logic is a bit flaky.  We pick the last IP in the list
+   * because I've seen nameservers from other network profiles still appear in
+   * the results from GetNetworkParams().  We make the assumption here that the
+   * last one is always valid.
+  */
   if (dwRetVal = GetNetworkParams(pFixedInfo, &ulOutBufLen) == NO_ERROR) {
-    x = pFixedInfo->DnsServerList.IpAddress.String;
+    r = pFixedInfo->DnsServerList.IpAddress.String;
+    pIPAddr = pFixedInfo->DnsServerList.Next;
+    while (pIPAddr) {
+      if (pIPAddr->IpAddress.String)
+        r = pIPAddr->IpAddress.String;
+      pIPAddr = pIPAddr->Next;
+    }
   } else {
-    return 0;
+    return NULL;
   }
 
   if (pFixedInfo)
     FREE(pFixedInfo);
 
-  return x;
+  return r;
 }
